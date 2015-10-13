5bab048620c9494d9e544835ba547f1d6bcf3483
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2015-10-13 10:13:40 -0400

    basics.h: removed unnecessary comments.

--- src/pome/basics.h
+++ src/pome/basics.h
@@ -4,7 +4,6 @@
 #include <kore/typedefs.h>
 #include <kore/array/array.h>
 #include <kore/array/array_linalg.h>
-//#include <Eigen/Core>
 
 namespace pome
 {
@@ -12,11 +11,5 @@ namespace pome
 typedef double Real;
 typedef std::complex<double> Complex;
 typedef kore::int64_t Integer;
-
-//typedef Eigen::VectorXcd ComplexVector;
-//typedef Eigen::VectorXd RealVector;
-//typedef Eigen::MatrixXcd ComplexMatrix;
-//typedef Eigen::MatrixXd RealMatrix;
-
 
 }
